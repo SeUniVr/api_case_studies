28267d02078a872945f5fe1d2bb4ca324d26fd83
amedeoz <amedeo.zampieri@studenti.univr.it>
2020-12-22 21:08:07 +0100

    bugfixes and sql init

--- live-streaming/mysql_init/init.sql
+++ live-streaming/mysql_init/init.sql
@@ -25,3 +25,17 @@ CREATE TABLE `videos` (
   KEY `fk_videos_users_idx` (`user_id`),
   CONSTRAINT `fk_videos_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO users VALUES
+(10, 'letizia', 'letizia', NULL, NULL),
+(11, 'plinio', 'plinio', NULL, NULL),
+(12, 'senzio', NULL, NULL, NULL),
+(13, 'tarquizio', NULL, NULL, NULL),
+(14, 'calliope', 'calliope', NULL, NULL);
+
+INSERT INTO videos VALUES
+(10, 10, 'Kill Will', 2.5, NULL, NULL, NULL),
+(11, 10, 'Kill Will 2', 4.5, 'kill_will_2_url', NULL, NULL),
+(12, 13, 'Kill Will 3', 3.5, NULL, NULL, NULL),
+(13, 13, 'Kill Will 4', 2.6, NULL, NULL, NULL),
+(14, 12, 'Kill Will 5', 110.0, NULL, NULL, NULL)
